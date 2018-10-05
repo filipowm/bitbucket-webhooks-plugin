b683c2e48f40465c1c766f57867a65b333bf4659
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2018-10-05 20:44:04 +0200

    made WebHookConfigurationModel package-private and immutable

--- src/main/java/nl/topicus/bitbucket/api/WebHookConfigurationModel.java
+++ src/main/java/nl/topicus/bitbucket/api/WebHookConfigurationModel.java
@@ -7,19 +7,19 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
-public class WebHookConfigurationModel {
+class WebHookConfigurationModel {
     @XmlElement
-    private Integer id;
+    private final Integer id;
     @XmlElement
-    private String title;
+    private final String title;
     @XmlElement
-    private String url;
+    private final String url;
     @XmlElement
-    private String committersToIgnore;
+    private final String committersToIgnore;
     @XmlElement
-    private String branchesToIgnore;
+    private final String branchesToIgnore;
     @XmlElement
-    private boolean enabled;
+    private final boolean enabled;
 
     WebHookConfigurationModel(WebHookConfiguration webHookConfiguration) {
         id = webHookConfiguration.getID();
@@ -30,55 +30,28 @@ public class WebHookConfigurationModel {
         enabled = webHookConfiguration.isEnabled();
     }
 
-    public WebHookConfigurationModel() {
-    }
-
-    public Integer getId() {
+    Integer getId() {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
+    String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUrl() {
+    String getUrl() {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getCommittersToIgnore() {
+    String getCommittersToIgnore() {
         return committersToIgnore;
     }
 
-    public void setCommittersToIgnore(String committersToIgnore) {
-        this.committersToIgnore = committersToIgnore;
-    }
-
-    public String getBranchesToIgnore() {
+    String getBranchesToIgnore() {
         return branchesToIgnore;
     }
 
-    public void setBranchesToIgnore(String branchesToIgnore) {
-        this.branchesToIgnore = branchesToIgnore;
-    }
-
-    public boolean isEnabled() {
+    boolean isEnabled() {
         return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
     }
 
     @Override
